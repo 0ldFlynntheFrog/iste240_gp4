2d244d6e258a29c93bdf098bc5a5dd4b1bc58fed
KasimRIT <klo7619@rit.edu>
2022-04-14 11:36:52 -0400

    Project stuff

---
+++ UnixAcademy/chapters.php
@@ -0,0 +1,19 @@
+<?php
+include("inc/heading.php");
+?>
+
+	<!-- Side Bar HTML below -->
+
+    <div class = "sidebar">
+
+        <a href = "link.html">Chapter 1</a>
+        <a href = "link.html">Chapter 2</a>
+        <a href = "link.html">Chapter 3</a>
+        <a href = "link.html">Chapter 3</a>
+
+    </div>
+
+	<!-- end of Side Bar -->
+
+</body>
+</html>
---
+++ UnixAcademy/index.php
@@ -0,0 +1,43 @@
+<?php
+include("inc/heading.php");
+?>
+
+	<!-- Content HTML below -->
+
+	<div class = "content">
+	<h1>What is <span style = "color: rgb(199, 49, 49)">Unix</span>?</h1>
+	<br>
+	<p>Unix is an Operating System that can be used to help the user interact wth the computer.
+		 Unix is also a multitasking program and was created in 1971. 
+		 Although the program is multitasking it wasnt meant for it to be like that at the begining.
+	</p>
+
+	<br>
+	<br>
+
+	<a class = "learn" href = "" >Learn More</a>
+
+	<br>
+	<br>
+
+	</div>
+
+	<!-- end of content -->
+
+	<br>
+	<br>
+	
+	<!-- Container HTML below -->
+
+	<div class = "container">
+		<div class = "box"></div>
+		<div class = "box"></div>
+		<div class = "box"></div>
+	</div>
+
+	<!-- end of container -->
+
+	
+	
+</body>
+</html>
