332c63bfc83fd401e0aead0638209418a1f14455
Danielle Antonacci <dja2135@g.rit.edu>
2022-04-18 20:21:43 -0400

    chapters

--- UnixAcademy/chapters.php
+++ UnixAcademy/chapters.php
@@ -7,15 +7,22 @@ include("assets/inc/heading.php");
 	<!-- Side Bar HTML below -->
 
     <div class = "sidebar">
-
+        <h1 class = "chapters">Unix Chapters</h1>
+        <br>
         <a href = "link.php">Chapter 1</a>
         <a href = "link.php">Chapter 2</a>
         <a href = "link.php">Chapter 3</a>
-        <a href = "link.php">Chapter 3</a>
+        <a href = "link.php">Chapter 4</a>
 
     </div>
 
 	<!-- end of Side Bar -->
+
+    <!-- Main HTML Below -->
+    <div class = "main" >
+       <!-- content for each chapter will go here -->
+    </div>
+    <!-- end of Main HTML -->
 
 </body>
 </html>
