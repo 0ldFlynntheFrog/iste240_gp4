0678b7c152b9cc49e3c37d2e3fb4d826e0b2501f
Danielle Antonacci <dja2135@g.rit.edu>
2022-04-21 11:39:20 -0400

    chapter chnages

--- UnixAcademy/chapters.php
+++ UnixAcademy/chapters.php
@@ -26,7 +26,7 @@ include("assets/inc/heading.php");
     <!-- Main HTML Below -->
     <div class = "main" >
         <!-- css for main and paragraph are not working -->
-        <!-- I was wondering if we should use javescript to chnage the information in the paragraph which is why I changed the links to buttons instead -->
+        <!-- I was wondering if we should use javascript to change the information in the paragraph which is why I changed the links to buttons instead -->
     <p class = "paragraph" style = "color: black; background-color: rgb(182, 189, 189)" >General Unix information goes here </p>
     </div>
     <!-- end of Main HTML -->
