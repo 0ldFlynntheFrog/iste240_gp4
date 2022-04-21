b6a75e3f4dd4504a0fbab2a165e96007bdccf85a
Danielle Antonacci <dja2135@g.rit.edu>
2022-04-21 12:05:42 -0400

    javascript chapters

--- UnixAcademy/chapters.php
+++ UnixAcademy/chapters.php
@@ -4,32 +4,50 @@ include("assets/inc/heading.php");
 
 	<link href = "assets/style.css" rel = 'stylesheet' >
 
+    <script src="unixacademy.js"></script>
+
 	<!-- Side Bar HTML below -->
 
     <div class = "sidebar">
-        <h1 class = "chapters">Unix Chapters</h1>
+        <!-- heading for side bar will also be a button -->
+        <button type="button" onclick="UnixChapters ()"> Unix Chapters</button>
+
         <br>
-        <!-- <a href = "link.php">Chapter 1</a>
-        <a href = "link.php">Chapter 2</a>
-        <a href = "link.php">Chapter 3</a>
-        <a href = "link.php">Chapter 4</a> -->
-        <button type="button" onclick="">Chapter 1</button>
-        <button type="button" onclick="">Chapter 2</button>
-        <button type="button" onclick="">Chapter 3</button>
-        <button type="button" onclick="">Chapter 4</button>
+        
+        <button type="button" onclick="Chapter1 ()">Chapter 1</button>
+        <button type="button" onclick="Chapter2 ()">Chapter 2</button>
+        <button type="button" onclick="Chapter3 ()">Chapter 3</button>
+        <button type="button" onclick="Chapter4 ()">Chapter 4</button>
         
 
     </div>
 
+    <style>
+        /* code is not working in style.css so I put it here for now */
+        .main {
+            background-color: rgb(182, 189, 189);
+            margin-left: 300px;
+            margin-right: 300px;
+            padding: 20px;
+        }
+  
+        .paragraph {
+            color: black;
+            font-size: 20px;
+        }
+    </style>
+
 	<!-- end of Side Bar -->
 
     <!-- Main HTML Below -->
-    <div class = "main" >
-        <!-- css for main and paragraph are not working -->
-        <!-- I was wondering if we should use javascript to change the information in the paragraph which is why I changed the links to buttons instead -->
-    <p class = "paragraph" style = "color: black; background-color: rgb(182, 189, 189)" >General Unix information goes here </p>
+
+    <!-- code changes with javascript when chapter buttons are clicked -->
+    <div class = "main"> 
+     <p class = "paragraph" id="paragraph" >General Unix Information goes here</p>
     </div>
     <!-- end of Main HTML -->
+
+  
 
 </body>
 </html>
---
+++ UnixAcademy/unixacademy.js
@@ -0,0 +1,24 @@
+function UnixChapters () {
+    var text = document.getElementById("paragraph")
+    text.innerHTML = "General Unix Information goes here"
+}
+
+function Chapter1 () {
+    var text = document.getElementById("paragraph")
+    text.innerHTML = "Chapter 1 information"
+}
+
+function Chapter2 () {
+    var text = document.getElementById("paragraph")
+    text.innerHTML = "Chapter 2 information"
+}
+
+function Chapter3 () {
+    var text = document.getElementById("paragraph")
+    text.innerHTML = "Chapter 3 information"
+}
+
+function Chapter4 () {
+    var text = document.getElementById("paragraph")
+    text.innerHTML = "Chapter 4 information"
+}
