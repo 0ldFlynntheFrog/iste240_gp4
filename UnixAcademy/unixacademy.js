76e06e8ec020fda5d0d10d4a85675ff76dbfc6e9
KasimRIT <klo7619@rit.edu>
2022-05-02 18:21:32 -0400

    Added a quiz, working on integrating it with PHP

---
+++ UnixAcademy/unixacademy.js
@@ -0,0 +1,10 @@
+function validateQuiz() {
+    var questionOneAnswer = document.forms["quiz"]["question1"].value;
+    var questionTwoAnswer = document.forms["quiz"]["question2"].value;
+    var questionThreeAnswer = document.forms["quiz"]["question3"].value;
+    
+    if(questionOneAnswer == "" || questionTwoAnswer == "" || questionThreeAnswer == ""){
+      document.getElementById("error").innerHTML = "Please answer all questions before submitting.";
+    }
+    console.log("Hit");
+}
