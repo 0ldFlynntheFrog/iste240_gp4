f7d3f763f3df18d5ab5d98410ba38a44aa9b21fc
KasimRIT <klo7619@rit.edu>
2022-05-02 22:29:50 -0400

    Changed some errors with labels

--- UnixAcademy/quiz1.php
+++ UnixAcademy/quiz1.php
@@ -13,38 +13,38 @@
 <fieldset>
             Question 1: True/False. Most operating systems are operating systems are based on Unix.<br>
             <input type="radio" id="q1a1" name="question1" value="True">
-            <label for="q3a1">True</label><br>
+            <label for="q1a1">True</label><br>
 
-            <input type="radio" id="q3a2" name="question1" value="False">
-            <label for="q3a2">False</label>
+            <input type="radio" id="q1a2" name="question1" value="False">
+            <label for="q1a2">False</label>
 </fieldset>
 <fieldset>
             Question 2: When was Unix orginally developed?<br>
             <input type="radio" id="q2a1" name="question2" value="1984">
-            <label for="q1a1">1984</label><br>
+            <label for="q2a1">1984</label><br>
 
             <input type="radio" id="q2a2" name="question2" <?php echo "value='" . $year . "'";?>>
-            <label for="q1a2"><?php echo $year; ?></label><br>
+            <label for="q2a2"><?php echo $year; ?></label><br>
 
             <input type="radio" id="q2a3" name="question2" value="1969">
-            <label for="q1a3">1969</label><br>
+            <label for="q2a3">1969</label><br>
     
             <input type="radio" id="q2a4" name="question2" value="Before the dawn of time">
-            <label for="q1a4">Before the dawn of time</label><br>
+            <label for="q2a4">Before the dawn of time</label><br>
 </fieldset>
 <fieldset>
             Question 3: What is the main importance for a terminal in operating systems?<br>
             <input type="radio" id="q3a1" name="question3" value="Serves as the home for Unix commands">
-            <label for="q2a1">Serves as the home for Unix commands</label><br>
+            <label for="q3a1">Serves as the home for Unix commands</label><br>
 
             <input type="radio" id="q3a2" name="question3" value="Helps us navigate our way around our computer">
-            <label for="q2a2">Helps us navigate our way around our computer</label><br>
+            <label for="q3a2">Helps us navigate our way around our computer</label><br>
 
             <input type="radio" id="q3a3" name="question3" value="Serves as communication between the user and computer">
-            <label for="q2a3">Serves as communication between the user and computer</label><br>
+            <label for="q3a3">Serves as communication between the user and computer</label><br>
     
             <input type="radio" id="q3a4" name="question3" value="All of the above">
-            <label for="q2a4">All of the above</label><br>
+            <label for="q3a4">All of the above</label><br>
 </fieldset>
   <p id="error"></p>
   <input type="submit" class="button" name="submit" value="Submit" />
